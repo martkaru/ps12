a75be40bf334781f03528add8dfc0424f204d003
Mart Karu <karu@metal.ee>
2012-04-30 18:24:11 +0300

    ülesanne 3.1 katse

---
+++ katsed/yl3.hs
@@ -0,0 +1,50 @@
+data Var = X | Y | Z deriving Eq
+
+data AExp = N Integer | V Var
+          | AExp :+ AExp | AExp :- AExp | AExp :* AExp
+          | Iff BExp AExp AExp
+
+data BExp = TT | FF | AExp :== AExp | AExp :<= AExp 
+          | Not BExp | BExp :&& BExp | BExp :|| BExp
+
+type State = [(Var, Integer)]
+
+lkp :: Var -> State -> Integer
+lkp x [] = 0
+lkp x ((y,z):s) = if x == y then z else lkp x s
+
+upd :: Var -> Integer -> State -> State
+upd x z [] = [(x,z)]
+upd x z ((y,w):s) = if x == y then (x,z):s
+                       else (y,w):upd x z s
+
+init :: State
+init = []
+
+show :: State -> String
+show = undefined
+
+aexp :: AExp -> State -> Integer
+aexp (N z) _      = z
+aexp (V x) s      = lkp x s
+aexp (a0 :+ a1) s = aexp a0 s + aexp a1 s
+aexp (a0 :- a1) s = aexp a0 s - aexp a1 s
+aexp (a0 :* a1) s = aexp a0 s * aexp a1 s
+aexp (Iff b0 a0 a1) s = if bexp b0 s then
+                          aexp a0 s
+                        else
+                          aexp a1 s
+
+bexp :: BExp -> State -> Bool
+bexp TT _ = True
+bexp FF _ = False
+bexp (a0 :== a1) s = aexp a0 s == aexp a1 s
+bexp (a0 :<= a1) s = aexp a0 s <= aexp a1 s
+bexp (Not b) s = not (bexp b s)
+bexp (a0 :&& a1) s = bexp a0 s && bexp a1 s
+bexp (a0 :|| a1) s = bexp a0 s || bexp a1 s
+
+testFunc :: AExp
+testFunc = (Iff ((N 3) :== (N 3)) (N 4) (N 2))
+
+test = aexp testFunc s where s = Main.init

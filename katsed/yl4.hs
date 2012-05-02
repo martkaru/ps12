d0bb81d246f2409eebea21e6fa057c14792656b7
Mart Karu <karu@metal.ee>
2012-05-02 19:06:30 +0300

    Ülesanne 4 haskellis

--- katsed/yl4.hs
+++ katsed/yl4.hs
@@ -2,13 +2,13 @@ data Var = X | Y | Z deriving Eq
 
 data AExp = N Integer | V Var
           | AExp :+ AExp | AExp :- AExp | AExp :* AExp
-          | Var := AExp
+          | Var ::= AExp
 
 data BExp = TT | FF | AExp :== AExp | AExp :<= AExp 
           | Not BExp | BExp :&& BExp | BExp :|| BExp
 
 data Stmt = Skip | Stmt :\ Stmt
-          {-| Var := AExp-}
+          | Var := AExp
           | If BExp Stmt Stmt
           | While BExp Stmt
 
@@ -40,38 +40,63 @@ zOf (z, s) = z
 aexp :: AExp -> State -> AResult
 aexp (N z) s      = (z, s)
 aexp (V x) s      = (lkp x s, s)
-aexp (x := a) s   = (z, upd x z s') where
+aexp (x ::= a) s   = (z, upd x z s') where
                           result = aexp a s
                           z = zOf result
                           s' = stateOf result
 aexp (a0 :+ a1) s = let first = aexp a0 s
                         last = aexp a1 (stateOf first)
                     in (zOf(first) + zOf(last), stateOf(last))
-{-aexp (a0 :- a1) s = aexp a0 s - aexp a1 s-}
-{-aexp (a0 :* a1) s = aexp a0 s * aexp a1 s-}
+aexp (a0 :- a1) s = let first = aexp a0 s
+                        last = aexp a1 (stateOf first)
+                    in (zOf(first) - zOf(last), stateOf(last))
+aexp (a0 :* a1) s = let first = aexp a0 s
+                        last = aexp a1 (stateOf first)
+                    in (zOf(first) * zOf(last), stateOf(last))
 
-{-bexp :: BExp -> State -> Bool-}
-{-bexp TT _ = True-}
-{-bexp FF _ = False-}
-{-bexp (a0 :== a1) s = aexp a0 s == aexp a1 s-}
-{-bexp (a0 :<= a1) s = aexp a0 s <= aexp a1 s-}
-{-bexp (Not b) s = not (bexp b s)-}
-{-bexp (a0 :&& a1) s = bexp a0 s && bexp a1 s-}
-{-bexp (a0 :|| a1) s = bexp a0 s || bexp a1 s-}
+type BResult = (Bool, State)
+stateOfB :: BResult -> State
+stateOfB (z, s) = s
+bOf :: BResult -> Bool
+bOf (b, s) = b
 
-{-stmt :: Stmt -> State -> State-}
-{-stmt Skip s = s-}
-{-stmt (stm0 :\ stm1) s = s' where-}
-                          {-s'' = stmt stm0 s-}
-                          {-s'  = stmt stm1 s''-}
-{-stmt (x := a) s = upd x z s where-}
-                          {-z = aexp a s-}
-{-stmt (If b stm0 stm1) s =-}
-    {-if bexp b s then-}
-      {-stmt stm0 s-}
-    {-else stmt stm1 s-}
-{-stmt (While b stm0) s =-}
-    {-if bexp b s then -}
-      {-stmt (While b stm0) (stmt stm0 s)-}
-    {-else s-}
-test = aexp((X := (N 1)) :+ (V X)) []
+bexp :: BExp -> State -> BResult
+bexp TT s = (True, s)
+bexp FF s = (False, s)
+bexp (a0 :== a1) s = let first = aexp a0 s
+                         last = aexp a1 (stateOf first)
+                     in (zOf(first) == zOf(last), stateOf(last))
+bexp (a0 :<= a1) s = let first = aexp a0 s
+                         last = aexp a1 (stateOf first)
+                     in (zOf(first) == zOf(last), stateOf(last))
+bexp (b0 :&& b1) s = let first = bexp b0 s
+                         last = bexp b1 (stateOfB first)
+                     in (bOf(first) && bOf(last), stateOfB(last))
+bexp (b0 :|| b1) s = let first = bexp b0 s
+                         last = bexp b1 (stateOfB first)
+                     in (bOf(first) || bOf(last), stateOfB(last))
+bexp (Not b) s = let result = bexp b s
+                 in (not (bOf result), stateOfB result)
+
+stmt :: Stmt -> State -> State
+stmt Skip s = s
+stmt (stm0 :\ stm1) s = s' where
+                          s'' = stmt stm0 s
+                          s'  = stmt stm1 s''
+stmt (x := a) s = upd x z s' where
+                          result = aexp a s
+                          z = zOf result
+                          s' = stateOf result
+stmt (If b stm0 stm1) s =
+    let result = bexp b s
+    in if bOf result then
+         stmt stm0 (stateOfB(result))
+       else stmt stm1 (stateOfB(result))
+
+stmt (While b stm0) s =
+    let result = bexp b s
+    in if bOf result then 
+         stmt (While b stm0) (stmt stm0 (stateOfB(result)))
+       else s
+
+test = aexp((X ::= ((N 1) :+ (V X))) :+ (V X)) []
